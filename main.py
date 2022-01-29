e1215402eb4e9fd1e33984988bbfbece24bf9365
danish <danishm306@gmail.com>
2022-01-29 17:58:13 +0530

    first selenium code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+DRIVER_PATH = './chromedriver'
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+
+from datetime import datetime
+nw = datetime.now()
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
+
+WebDriverWait(driver, timeout=5)
+
+driver.get("https://www.google.com")
+print(driver.title) # => "Google"
